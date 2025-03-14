6603ae82364f51d1ef17afca999bd5d8a3b7b54e
JoyTarafder <joytarafder3@gmail.com>
2025-03-15 00:26:14 +0600

    update error

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,18 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		allowedHosts: true,
-	},
+  plugins: [react()],
+  server: {
+    allowedHosts: true,
+  },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom", "react-router-dom"],
+        },
+      },
+    },
+  },
 });
